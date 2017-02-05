50b41942fbcc89a41b89aa394fb840bf5dc39e76
Shurong Gu <jacqueline@ShurongdeMacBook-Pro.local>
2017-02-05 15:14:05 -0600

    bridges after 2000

--- Bridgedata.R
+++ Bridgedata.R
@@ -3,7 +3,7 @@ library(choroplethr)
 library(dplyr)
 library(readr)
 library(data.table)
-library(ggplot)
+library(ggplot2)
 
 dest = "https://www.fhwa.dot.gov/bridge/nbi/2016/delimited/AK16.txt"
 tmp = fread(dest) 
@@ -23,9 +23,8 @@ dest= rep("", 52)
 for(i in 1:52) dest[i]=paste("https://www.fhwa.dot.gov/bridge/nbi/2016/delimited/", states[i,2],"16.txt", sep = "") 
 x16 =ldply(dest, fread, colClasses = classes)  
 
-save(x16, file = "bridgesdata.RData")
 keep = c("STATE_CODE_001",   "STRUCTURE_NUMBER_008" , "COUNTY_CODE_003", "LAT_016", "LONG_017",
-         "YEAR_BUILT_027", "DECK_COND_058")
+         "YEAR_BUILT_027")
 M=x16
 M=as.tbl(M)
 data16=select(M, one_of(keep))
